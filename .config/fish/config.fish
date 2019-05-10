b53340672dc90aa78fdc84eb06efb7adc3811746
Rowan Skewes <rowan.skewes@gmail.com>
2019-05-10 10:00:35 +1000

    fish config update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,7 +6,7 @@ alias rm='rm -i'
 
 set -gx EDITOR vim
 
-set -gx PATH $HOME/.psvm/current/bin $PATH
+#set -gx PATH $HOME/.psvm/current/bin $PATH
 
 alias cat=bat
 
@@ -16,6 +16,7 @@ set -gx PATH $HOME/sneaky_non_nix_builds/freeplane $PATH
 
 alias CloudCompare="env LIBGL_ALWAYS_SOFTWARE=true CloudCompare"
 alias firefox="env LIBGL_ALWAYS_SOFTWARE=true firefox"
+alias meshlab="env LIBGL_ALWAYS_SOFTWARE=true meshlab"
 
 function background --description "Run a fish command in the background using a subshell, thanks to pirate https://github.com/fish-shell/fish-shell/issues/238#issuecomment-302897122"
     fish -c "fish -c '""$argv""' &"
